79c2d0bf859369f651d16f01e37b357787391b57
Vladimiro-Mário-a22302893 <79208994+a22000686@users.noreply.github.com>
2026-05-11 12:45:41 +0100

    adicionando readme

---
+++ readme.m
@@ -0,0 +1,15 @@
+# Projeto Exemplo Git
+
+👋 Seja bem-vindo ao repositório!
+
+Este projeto é um exemplo simples para aprender a usar Git e GitHub.
+
+## 🚀 Funcionalidades
+
+- Utilização do Git
+- Utilização do GitHub
+- Escrita em Markdown
+- Criação de commits e branches
+
+## 📦 Estrutura do Projeto
+
